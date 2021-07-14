b48950ebf67473e28d8bb498378ab8e5b1943cf2
seokjoon <sdeokjoon@gmail.com>
2021-07-14 22:15:15 +0900

    api resource test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Route::get('/', function () { return view('welcome'); });
-Route::get('/', function () { return File::get(public_path('react-test-r3/index.html')); });
+Route::get('/', function () { return view('mr1'); });
+//Route::get('/', function () { return File::get(public_path('react-test-r3/index.html')); });
 
 //////// vue BEGIN
 Route::any('/react-test-r3/{path}', function () { return File::get(public_path('react-test-r3/index.html')); });
